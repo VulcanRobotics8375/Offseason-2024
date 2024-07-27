a0cd19675f3c9da6eff7f453de2ddac19e6d79c3
leia <aleia.pei123@gmail.com>
2024-07-26 18:53:44 -0700

    added roadrunner + simpleAutonomous file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBoard1.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.mechanisms;
-
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-public class ProgrammingBoard1 {
-}
